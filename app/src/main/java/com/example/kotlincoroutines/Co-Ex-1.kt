b882fb9616312766839ca892ce78bb09c33c191f
aditi <aditi.tantradnyan@gmail.com>
2021-05-13 18:44:21 +0530

    Added 2 coroutines in supend function

--- app/src/main/java/com/example/kotlincoroutines/Co-Ex-1.kt
+++ app/src/main/java/com/example/kotlincoroutines/Co-Ex-1.kt
@@ -9,15 +9,25 @@ fun main () = runBlocking { //this: CoroutineScope
      */
 
     printMessage()
+    println("Done!")
 
  }
+/*
+A coroutineScope builder canbe used inside any suspending function to perform multiple
+concurrent operations. Let's launch 2 concurrent coroutines inside a printMessage suspending
+function
+ */
 
 private suspend fun printMessage() = coroutineScope {
     launch {
         // launch a new coroutine and continue
-        delay(1000L) // non-blocking delay for 1 sec
+        delay(2000L) // non-blocking delay for 1 sec
         //default time unit is ms
-        println("World") //Print after delay
+        println("World2") //Print after delay
+    }
+    launch {
+        delay(1000L)
+        println("World1")
     }
     println("Hello") // main coroutine continues while a previous one is delayed
 }
@@ -27,5 +37,11 @@ Hello gets printed and then after some delay World gets printed
 Output
 Hello
 World
+
+2 coroutines output
+Hello
+World1
+World2
+Done!
 
  */
