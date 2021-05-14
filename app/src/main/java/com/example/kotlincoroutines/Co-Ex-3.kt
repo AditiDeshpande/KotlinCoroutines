78342dcc64cecd7bd6fa5d44fc62453bd5e73b29
aditi <aditi.tantradnyan@gmail.com>
2021-05-14 15:28:14 +0530

    Coroutine vs Threads

--- app/src/main/java/com/example/kotlincoroutines/Co-Ex-3.kt
+++ app/src/main/java/com/example/kotlincoroutines/Co-Ex-3.kt
@@ -3,8 +3,9 @@ package com.example.kotlincoroutines
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
 
-fun main() = runBlocking {
+/*fun main() = runBlocking {
     repeat(100_000){
         //Launch a lot of coroutines
         launch {
@@ -12,10 +13,31 @@ fun main() = runBlocking {
             print(".")
         }
     }
-}
+}*/
 /*
 Output
 It launches 100K coroutines and, after 5 seconds,
 each coroutine prints a dot.
  */
 
+//Now with Threads
+
+fun main() =
+    repeat(100_000){
+        //Launch a lot of coroutines
+        thread {
+            Thread.sleep(5000L)
+            print(".")
+        }
+    }
+/*
+Exception in thread "main" java.lang.OutOfMemoryError: unable to create new native thread
+	at java.lang.Thread.start0(Native Method)
+	at java.lang.Thread.start(Thread.java:717)
+	at kotlin.concurrent.ThreadsKt.thread(Thread.kt:42)
+	at kotlin.concurrent.ThreadsKt.thread$default(Thread.kt:25)
+	at com.example.kotlincoroutines.Co_Ex_3Kt.main(Co-Ex-3.kt:28)
+	at com.example.kotlincoroutines.Co_Ex_3Kt.main(Co-Ex-3.kt)
+......................................................................................................
+After the exception..... at the end started printing the dots
+ */
